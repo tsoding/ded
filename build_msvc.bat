9c8f48e2a1655bccd252dd2c3de3a7092ce8e05f
rexim <reximkut@gmail.com>
2021-07-02 21:34:42 +0700

    Try to fix MSVC build again

--- build_msvc.bat
+++ build_msvc.bat
@@ -5,4 +5,4 @@ set CFLAGS=/W4 /WX /std:c11 /FC /TC /Zi /nologo
 set INCLUDES=/I SDL2\include
 set LIBS=SDL2\lib\x64\SDL2.lib SDL2\lib\x64\SDL2main.lib Shell32.lib
 
-cl.exe %CFLAGS% %INCLUDES% /Fete main.c la.c /link %LIBS% -SUBSYSTEM:windows
+cl.exe %CFLAGS% %INCLUDES% /Fete main.c la.c editor.c /link %LIBS% -SUBSYSTEM:windows
