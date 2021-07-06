0459c19f0779360dad6953d649f5d3f900f1508f
Danil Kolumbet <kolumbetko@gmail.com>
2021-07-07 02:47:14 +0300

    (#16) Remove duplicated code from build_msvc.bat

--- build_msvc.bat
+++ build_msvc.bat
@@ -10,7 +10,4 @@ set LIBS=dependencies\SDL2\lib\x64\SDL2.lib ^
          dependencies\GLEW\lib\glew32s.lib ^
          opengl32.lib User32.lib Gdi32.lib Shell32.lib
 
-if not exist build\ mkdir build\
-if not exist build\SDL2.dll copy dependencies\SDL2\lib\x64\SDL2.dll build\SDL2.dll
-
 cl.exe %CFLAGS% %INCLUDES% %BASE_FILES% /Fobuild\ /Febuild\ded /link %LIBS% -SUBSYSTEM:windows
