41efbc71ac2af3399de7942930cea294cc534209
Stay <39851578+StayWithMeSenpai@users.noreply.github.com>
2023-01-12 20:10:37 +0100

    Update setup_dependencies.bat
    Add "--ssl-no-revoke", because some antivirus software breaks curl through custom certificates and this fixes that

--- setup_dependencies.bat
+++ setup_dependencies.bat
@@ -1,6 +1,6 @@
 @echo off
 
-curl -fsSL -o SDL2-devel-2.0.12-VC.zip https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip
+curl -fsSL -o SDL2-devel-2.0.12-VC.zip https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip --ssl-no-revoke
 tar -xf SDL2-devel-2.0.12-VC.zip
 if not exist dependencies\ mkdir dependencies\
 move SDL2-2.0.12 dependencies\SDL2
@@ -9,7 +9,7 @@ if not exist dependencies\SDL2\temp\ mkdir dependencies\SDL2\temp\
 move dependencies\SDL2\include dependencies\SDL2\temp\SDL2
 move dependencies\SDL2\temp dependencies\SDL2\include
 
-curl -fsSL -o glfw-3.3.2.bin.WIN64.zip https://github.com/glfw/glfw/releases/download/3.3.2/glfw-3.3.2.bin.WIN64.zip
+curl -fsSL -o glfw-3.3.2.bin.WIN64.zip https://github.com/glfw/glfw/releases/download/3.3.2/glfw-3.3.2.bin.WIN64.zip --ssl-no-revoke
 tar -xf glfw-3.3.2.bin.WIN64.zip
 if not exist dependencies\GLFW\lib\ mkdir dependencies\GLFW\lib\
 move glfw-3.3.2.bin.WIN64\lib-vc2019\glfw3.lib dependencies\GLFW\lib\glfw3.lib
@@ -18,7 +18,7 @@ move glfw-3.3.2.bin.WIN64\include\GLFW\glfw3.h dependencies\GLFW\include\GLFW\gl
 del glfw-3.3.2.bin.WIN64.zip
 rmdir /s /q glfw-3.3.2.bin.WIN64
 
-curl -fsSL -o glew-2.1.0-win32.zip https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0-win32.zip/download
+curl -fsSL -o glew-2.1.0-win32.zip https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0-win32.zip/download --ssl-no-revoke
 tar -xf glew-2.1.0-win32.zip
 if not exist dependencies\GLEW\lib\ mkdir dependencies\GLEW\lib\
 move glew-2.1.0\lib\Release\x64\glew32s.lib dependencies\GLEW\lib\glew32s.lib
