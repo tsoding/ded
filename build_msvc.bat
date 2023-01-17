863c66888fc15f927bfb2da42419623a39a8cbf2
Roderic Bos <github@rooc.nl>
2023-01-17 11:28:50 +0100

    added lexer to build

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,7 +1,7 @@
 @echo off
 rem launch this from msvc-enabled console
 
-set CFLAGS=/W4 /WX /std:c11 /wd4996 /wd5105 /wd4459 /wd4267 /wd4244 /wd4200 /wd4090 /FC /TC /Zi /nologo
+set CFLAGS=/W4 /WX /std:c11 /wd4996 /wd5105 /wd4459 /wd4267 /wd4244 /wd4200 /wd4090 /wd4702 /FC /TC /Zi /nologo
 set INCLUDES=/I dependencies\SDL2\include /I dependencies\GLFW\include /I dependencies\GLEW\include /I dependencies\freetype2\include
 set LIBS=dependencies\SDL2\lib\x64\SDL2.lib ^
          dependencies\SDL2\lib\x64\SDL2main.lib ^
@@ -10,4 +10,4 @@ set LIBS=dependencies\SDL2\lib\x64\SDL2.lib ^
          "dependencies\freetype2\release dll\win64\freetype.lib" ^
          opengl32.lib User32.lib Gdi32.lib Shell32.lib
 
-cl.exe %CFLAGS% %INCLUDES% /Feded src\main.c src\la.c src\editor.c src\file_browser.c src\free_glyph.c src\simple_renderer.c src\common.c src\free_glyph.c src\simple_renderer.c /link %LIBS% -SUBSYSTEM:console
+cl.exe %CFLAGS% %INCLUDES% /Feded src\main.c src\la.c src\editor.c src\file_browser.c src\free_glyph.c src\simple_renderer.c src\common.c src\free_glyph.c src\simple_renderer.c src\lexer.c /link %LIBS% -SUBSYSTEM:console
