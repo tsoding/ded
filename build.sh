2ee096b1046a04b54ce21e4f8ac2b69490dba927
Eddy Jansson <eddy@klopper.net>
2023-01-29 00:09:48 +0100

    Un-inline first pkg-config call.
    This makes it so that, if any library is missing, the
    script stops there instead of trying to compile the code,
    which is going to fail anyway and spew potentially
    misleading error messages to the console. (GH-52)

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ PKGS="sdl2 glew freetype2"
 CFLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb"
 LIBS=-lm
 SRC="src/main.c src/la.c src/editor.c src/file_browser.c src/free_glyph.c src/simple_renderer.c src/common.c src/lexer.c"
+PKG_CFLAGS=`pkg-config --cflags $PKGS`
 
 if [ `uname` = "Darwin" ]; then
     CFLAGS+=" -framework OpenGL"
 fi
 
-$CC $CFLAGS `pkg-config --cflags $PKGS` -o ded $SRC $LIBS `pkg-config --libs $PKGS`
+$CC $CFLAGS $PKG_CFLAGS -o ded $SRC $LIBS `pkg-config --libs $PKGS`
