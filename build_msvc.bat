266a20d9f4614e3a6c8328a8182e95041425a0bc
Lorenz Lutz <lorenz@mklutz.de>
2023-01-29 07:33:39 +0100

    update windows build scripts

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,16 +1,31 @@
 @echo off
+cd /D "%~dp0"
 rem launch this from msvc-enabled console
 
-set CFLAGS=/W4 /WX /std:c11 /wd4996 /wd5105 /wd4244 /wd4267 /wd4702 /wd4200 /wd4090 /FC /TC /Zi /nologo
+set CFLAGS=/W4 /WX /std:c11 /wd4090 /wd4200 /wd4244 /wd4267 /wd4702 /wd4996 /wd5105 /FC /TC /Zi /nologo
 set LDFLAGS=/ignore:4099 /subsystem:windows
-set INCLUDES=/I dependencies\SDL2\include /I dependencies\GLFW\include /I dependencies\GLEW\include /I dependencies\freetype\include
-set LIBS=dependencies\SDL2\lib\x64\SDL2.lib ^
-         dependencies\SDL2\lib\x64\SDL2main.lib ^
-         dependencies\GLFW\lib\glfw3.lib ^
+
+set INCLUDES=/I dependencies\freetype\include ^
+             /I dependencies\GLEW\include ^
+             /I dependencies\GLFW\include ^
+             /I dependencies\SDL2\include
+
+set LIBS=dependencies\freetype\lib\freetype.lib ^
          dependencies\GLEW\lib\glew32s.lib ^
-         dependencies\freetype\lib\freetype.lib ^
+         dependencies\GLFW\lib\glfw3.lib ^
+         dependencies\SDL2\lib\x64\SDL2main.lib ^
+         dependencies\SDL2\lib\x64\SDL2.lib ^
          opengl32.lib User32.lib Gdi32.lib Shell32.lib
 
-if not exist SDL2.dll copy dependencies\SDL2\lib\x64\SDL2.dll SDL2.dll || exit /b
+set SRC=src\common.c ^
+        src\editor.c ^
+        src\file_browser.c ^
+        src\free_glyph.c ^
+        src\la.c ^
+        src\lexer.c ^
+        src\main.c ^
+        src\simple_renderer.c
 
-cl.exe %CFLAGS% %INCLUDES% /Feded src\common.c src\editor.c src\file_browser.c src\free_glyph.c src\la.c src\lexer.c src\main.c src\simple_renderer.c /link %LIBS% %LDFLAGS%
+if not exist SDL2.dll copy dependencies\SDL2\lib\x64\SDL2.dll SDL2.dll > nul || exit /b
+
+cl.exe %CFLAGS% %INCLUDES% /Feded %SRC% /link %LDFLAGS% %LIBS%
--- setup_dependencies.bat
+++ setup_dependencies.bat
@@ -1,40 +1,41 @@
 @echo off
+cd /D "%~dp0"
 
-if not exist dependencies\ mkdir dependencies\
-
+echo Setting up SDL2...
 curl -fsSL -o SDL2-devel-2.0.12-VC.zip https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip
 tar -xf SDL2-devel-2.0.12-VC.zip
-if not exist dependencies\SDL2 mkdir dependencies\SDL2
-if not exist dependencies\SDL2\include mkdir dependencies\SDL2\include
 if not exist dependencies\SDL2\include\SDL2 mkdir dependencies\SDL2\include\SDL2
-move SDL2-2.0.12\include\* dependencies\SDL2\include\SDL2\
-move SDL2-2.0.12\lib dependencies\SDL2\
+move /y SDL2-2.0.12\include\* dependencies\SDL2\include\SDL2\ > nul
+move /y SDL2-2.0.12\lib dependencies\SDL2\lib > nul
 del SDL2-devel-2.0.12-VC.zip
 rmdir /s /q SDL2-2.0.12
 
+echo Setting up GLFW...
 curl -fsSL -o glfw-3.3.2.bin.WIN64.zip https://github.com/glfw/glfw/releases/download/3.3.2/glfw-3.3.2.bin.WIN64.zip
 tar -xf glfw-3.3.2.bin.WIN64.zip
 if not exist dependencies\GLFW\lib\ mkdir dependencies\GLFW\lib\
-move glfw-3.3.2.bin.WIN64\lib-vc2019\glfw3.lib dependencies\GLFW\lib\glfw3.lib
+move /y glfw-3.3.2.bin.WIN64\lib-vc2019\glfw3.lib dependencies\GLFW\lib\glfw3.lib > nul
 if not exist dependencies\GLFW\include\GLFW mkdir dependencies\GLFW\include\GLFW
-move glfw-3.3.2.bin.WIN64\include\GLFW\glfw3.h dependencies\GLFW\include\GLFW\glfw3.h
+move /y glfw-3.3.2.bin.WIN64\include\GLFW\glfw3.h dependencies\GLFW\include\GLFW\glfw3.h > nul
 del glfw-3.3.2.bin.WIN64.zip
 rmdir /s /q glfw-3.3.2.bin.WIN64
 
+echo Setting up GLEW...
 curl -fsSL -o glew-2.1.0-win32.zip https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0-win32.zip/download
 tar -xf glew-2.1.0-win32.zip
 if not exist dependencies\GLEW\lib\ mkdir dependencies\GLEW\lib\
-move glew-2.1.0\lib\Release\x64\glew32s.lib dependencies\GLEW\lib\glew32s.lib
+move /y glew-2.1.0\lib\Release\x64\glew32s.lib dependencies\GLEW\lib\glew32s.lib > nul
 if not exist dependencies\GLEW\include\GL\ mkdir dependencies\GLEW\include\GL\
-move glew-2.1.0\include\GL\glew.h dependencies\GLEW\include\GL\glew.h
+move /y glew-2.1.0\include\GL\glew.h dependencies\GLEW\include\GL\glew.h > nul
 del glew-2.1.0-win32.zip
 rmdir /s /q glew-2.1.0
 
+echo Setting up freetype...
 curl -fsSL -o freetype-windows-binaries-2.12.1.zip https://github.com/ubawurinna/freetype-windows-binaries/archive/refs/tags/v2.12.1.zip
 tar -xf freetype-windows-binaries-2.12.1.zip
 if not exist dependencies\freetype mkdir dependencies\freetype
-move freetype-windows-binaries-2.12.1\include dependencies\freetype\
+move /y freetype-windows-binaries-2.12.1\include dependencies\freetype\include > nul
 if not exist dependencies\freetype\lib mkdir dependencies\freetype\lib
-move "freetype-windows-binaries-2.12.1\release static\vs2015-2022\win64\freetype.lib" dependencies\freetype\lib\
+move /y "freetype-windows-binaries-2.12.1\release static\vs2015-2022\win64\freetype.lib" dependencies\freetype\lib\freetype.lib > nul
 del freetype-windows-binaries-2.12.1.zip
 rmdir /s /q freetype-windows-binaries-2.12.1
